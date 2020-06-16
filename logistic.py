635b6a14862d18fd81d728969480da6970a0604a
Pratyush Singh <spratyush02@gmail.com>
2020-06-17 01:34:32 +0200

    again

--- logistic.py
+++ logistic.py
@@ -1,6 +1,41 @@
+#%%
 import pandas as pd, numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
+import re, string
 
-train = pd.read_csv('..\data\train.csv')
+train = pd.read_csv('data/train.csv')
+test = pd.read_csv('data/test.csv')
+#%%
+
+train.head()
+
+# %%
+label_cols = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
+train['none'] = 1 - train[label_cols].max(axis=1)
+train.describe()
+# %%
+
+len(train),len(test)
+
+COMMENT = 'comment_text'
+train[COMMENT].fillna("unknown", inplace=True)
+test[COMMENT].fillna("unknown", inplace=True)
+
+
+# %%
+re_tok = re.compile(f'([{string.punctuation}“”¨«»®´·º½¾¿¡§£₤‘’])')
+def tokenize(s): return re_tok.sub(r' \1 ', s).split()
+
+
+
+n = train.shape[0]
+vec = TfidfVectorizer(ngram_range=(1,2), tokenizer=tokenize,
+               min_df=3, max_df=0.9, strip_accents='unicode', use_idf=1,
+               smooth_idf=1, sublinear_tf=1 )
+trn_term_doc = vec.fit_transform(train[COMMENT])
+test_term_doc = vec.transform(test[COMMENT])
+
+
+# %%
